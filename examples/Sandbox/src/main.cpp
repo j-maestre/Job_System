fc7e832ffd8455ee5c956d4f625a8dc15e5b05a8
j-maestre <xemaiestacio@gmail.com>
2023-10-12 13:15:25 +0200

    add easy and medium functions

--- examples/Sandbox/src/main.cpp
+++ examples/Sandbox/src/main.cpp
@@ -1,42 +1,83 @@
 #include <stdio.h>
 #include <iostream>
+#include <chrono>
 #include "JobSystem.h"
 
 
 void Easy(){
+  auto inicio = std::chrono::high_resolution_clock::now();
 
-  printf("yeeeee");
   // 5 millones
-  const int max = 5;
-  for(int i = 0; i < max; i++){
-    std::cout << " num: " << i;
+  long long int acum = 0;
+  const int max = 10000;
+  for (int i = 0; i < max; i++){
+    for (int j = 0; j < max; j++){
+      acum += j + i;
+    }
+    
   }
+  
+  auto fin = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duracion = fin - inicio;
+  double duracion_ms = duracion.count() * 1000.0;
+
+  printf("Task Easy done in %fms\n", duracion_ms);
 }
 
 
 void Medium(){
 
-  const int max = 128;
+  auto inicio = std::chrono::high_resolution_clock::now();
+
+  const int range = 50000;
+  // Calcular si un numero es primo (divisible solo entre el mismo y 1)
+  for (int i = 0; i < range; i++){
+    
+    bool divisible = false;
+
+    for(int j = 2; j < i; j++){
+      // Es divisible entre un numero entre medias
+      if(i % j == 0){
+       divisible = true;
+      }
+    }
+    //if(!divisible)printf("%d es primo\n",i);
+  }
+
+  auto fin = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duracion = fin - inicio;
+  double duracion_ms = duracion.count() * 1000.0;
+
+  printf("Task Medium done in %fms\n", duracion_ms);
+
+
   
-  char caracteres[max];
-
-  for (int i = 0; i < max; ++i) {
-    caracteres[i] = static_cast<char>(i);
-  }
-
-  for(int i = 0 ; i < max; i++){
-    std::cout << "char = " << caracteres[i];
-  }
+  
 }
 
 
-int main(){
-
+int main(int argc, char *argv[]){
   JobSystem job;
 
   //Easy();
-  //job.add_task(Easy);
+  std::function<void()> func1{Easy};
+  std::function<void()> func2{Medium};
+  
+
+  for (int i = 0; i < 30; i++){
+    job.add_task(func2);
+    job.add_task(func1);
+    //job.add_task(func2);
+    //job.add_task(func1);
+    //job.add_task(func2);
+    //job.add_task(func2);
+  }
+
+  job.wait_until_finish();
   //job.add_task(Medium);
 
+
+
+  // No hace las 30 porque llamo al destructor cuando aun siguen habiendo tareas en la cola
   return 0;
 }
